bf2c24c5641586da9a3c553879ab7fcf59cd3322
BruhNav <pranav20100@iiitnr.edu.in>
2022-11-29 13:43:53 +0530

    edited frontend

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -37,13 +37,13 @@ const handleSubmit = (e) => {
 }
 
     return(
-      <div><Navbar/>
+      <div>
       <MDBContainer fluid className="p-3 my-5 h-custom">
   
         <MDBRow>
   
           <MDBCol col='10' md='5'>
-            <img src="https://images.unsplash.com/photo-1551218808-94e220e084d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" class="img-fluid" alt="Sample image" />
+            <img src={require('../images/side.jpg')} class="img-fluid" alt="Sample image" />
           </MDBCol>
   
           <MDBCol style={{padding:20}} col='4' md='6'>
--- client/src/components/main.js
+++ client/src/components/main.js
@@ -2,41 +2,35 @@ import React from "react";
 import { Row, Col, Button, Badge , h1 , h2 , h3 } from "reactstrap";
 import Navbar from "./navbar";
 import { useNavigate } from 'react-router-dom'
+import '../styles/sliderstyles.css'
+import {IoSparklesSharp} from 'react-icons/io5'
 
 export default props => {
     const navigate = useNavigate()
 
     return (
-      <div >
-      <Navbar />
-        <Row noGutters className="text-center align-items-center pizza-cta">
-          <Col>
-            <p style={{padding:35}}>
-              {/* Welcome , to TATVA by Taj */}
-              <h3>
-            Welcome to, TATVA {' '}
-              <Badge>
-              by Taj
-              </Badge>
-          </h3>
-            </p>
-            <Button
+      <div className="slider">
+        <input type="radio" name="slider" defaultChecked="true" />
+        <div className="slider__content">
+          <img src={require("../images/1.jpg")} />
+          <div className="slider__description">
+            <h1><IoSparklesSharp/>Hotel New Raipur<IoSparklesSharp/></h1>
+            <p>Life is short, make it sweet.</p>
+            <button
+              className="booknow"
               onClick={()=>navigate("/login")} style={{margin:17 , padding:10}}
-            >
-              Book a Table
-            </Button>
-          </Col>
-        </Row>
-        <Row noGutters className="text-center big-img-container">
-          <Col>
-            <img
-              src={require("../images/cafe.jpg")}
-              alt="cafe"
-              className="big-img"
-              width={1000}
-            />
-          </Col>
-        </Row>
+            >Book Now</button>
+          </div>
+        </div>
+        <input type="radio" name="slider" />
+        <div className="slider__content">
+          <img src={require("../images/2.jpg")} />
+          <div className="slider__description">
+            <p>Log In or Sign Up for exicting coupons and discounts!</p>
+            <button
+              className="booknow">Log In/ Sign Up</button>
+          </div>
+        </div>
       </div>
     );
   };
